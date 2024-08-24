5a67ee78fc0a608392e9c6df83c43e8f706ee276
Willipatas <wgmedina@gmail.com>
2024-08-23 20:55:41 -0500

    Se agrega la función post para crear categorias

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Express, { Router } from "express";
+import bodyParser from "body-parser";
 import pool from "./database/databaseconnect";
-import { getCategories, getCategoriesById } from "./controllers/categories_controllers";
+import { createCategories, getCategories, getCategoriesById } from "./controllers/categories_controllers";
 
 require('dotenv').config();
 
@@ -9,8 +10,12 @@ const port = process.env.PORT;
 
 const categoriesRouter = Router();
 
+app.use(Express.json());
+
+
 categoriesRouter.get('/categories', getCategories);
 categoriesRouter.get('/categories/:id', getCategoriesById);
+categoriesRouter.post('/createCategories', createCategories);
 
 
 app.use(categoriesRouter);
--- src/controllers/categories_controllers.ts
+++ src/controllers/categories_controllers.ts
@@ -37,3 +37,27 @@ export const getCategoriesById = async (req: Request, res:Response): Promise<Res
     }
 };
 
+export const createCategories = async (req: Request, res:Response): Promise<Response> => {
+    const {categoryId, categoryName, categoryDescription} = req.body;
+
+    if (categoryId !== null && categoryName !== null && categoryDescription !== null) {
+        try {
+            await pool.query('INSERT INTO categories (category_id, category_name, description) values ($1, $2, $3)',
+                [categoryId, categoryName, categoryDescription]
+            );
+            return res.status(201).json({
+                message: 'Categoria creada satisfactoriamente',
+                category: {
+                    categoryId,
+                    categoryName,
+                    categoryDescription,
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json('Internal Server Error');
+        }
+    } else {
+        return res.status(500).json('Internal Server Error');
+    }
+};
